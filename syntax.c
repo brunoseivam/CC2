428d2cc98bc419e90bc9563eea17928e338ea0cd
Lucas Russo <lerwys@gmail.com>
2011-10-14 00:21:10 -0300

    Inserção dos primeiros X

--- syntax.c
+++ syntax.c
@@ -50,6 +50,23 @@ const firsts identificador_firsts =
    .other_firsts_list_size = 0
 };
 
+/* Autor: Acosta
+   primeiros(?) = {?}
+*/
+const firsts xis =
+{
+   .string_list      = (const char * const []){"algoritmo"},
+   .string_list_size = 1,
+
+   /*.tk_class_list    = (const token_class []){real_number, integer_number},
+   .tk_class_list_size = 2*/
+
+   .tk_class_list    = NULL,
+   .tk_class_list_size = 0,
+
+   .other_firsts_list = (const firsts * const []){ &declaracao_global_firsts, &declaracao_local_firsts },
+   .other_firsts_list_size = 2
+};
 
 
 /*
